8c9bb73e384ed612bd56b79b15f35eb95e8d741a
pranav <pranav0492@gmail.com>
2024-11-25 23:02:51 +0000

    add edit delete functionality through modal

--- app/Livewire/SummaryTable.php
+++ app/Livewire/SummaryTable.php
@@ -12,10 +12,65 @@ class SummaryTable extends Component
 
     public $search = '';
     public $perPage = 10;
-    public function updatingSearch()
+    public $isModalOpen = false;
+
+    public $editId;
+    public $phone;
+    public $email;
+    public $notes;
+
+    protected $rules = [
+        'phone' => 'required|min:10',
+        'email' => 'required|email',
+        'notes' => 'required|min:3'
+    ];
+
+    // Add real-time validation
+    public function updated($propertyName)
     {
-        $this->resetPage();
+        $this->validateOnly($propertyName);
     }
+
+    public function edit($summaryId)
+    {
+        $this->resetValidation();
+        $summary = Summary::findOrFail($summaryId);
+
+        $this->editId = $summary->id;
+        $this->phone = $summary->phone;
+        $this->email = $summary->email;
+        $this->notes = $summary->notes;
+
+        $this->isModalOpen = true;
+    }
+
+    public function update()
+    {
+        $this->validate();
+
+        Summary::find($this->editId)->update([
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'notes' => $this->notes,
+        ]);
+
+        $this->closeModal();
+        session()->flash('message', 'Summary updated successfully.');
+    }
+
+    public function delete()
+    {
+        Summary::find($this->editId)->delete();
+        $this->closeModal();
+        session()->flash('message', 'Summary deleted successfully.');
+    }
+
+    public function closeModal()
+    {
+        $this->isModalOpen = false;
+        $this->resetValidation();
+    }
+
     public function render()
     {
         return view('livewire.summary-table', [
