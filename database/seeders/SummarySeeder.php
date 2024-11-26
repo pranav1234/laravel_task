9c966a5a0eb1161d6e1406e512ce1e48350b67b2
pranav <pranav0492@gmail.com>
2024-11-26 11:23:29 +0000

    update seeder

--- database/seeders/SummarySeeder.php
+++ database/seeders/SummarySeeder.php
@@ -4,16 +4,19 @@ namespace Database\Seeders;
 
 use App\Models\Summary;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class SummarySeeder extends Seeder
 {
     public function run()
     {
+        $faker = Faker::create();
+
         for ($i = 0; $i < 50; $i++) {
             Summary::create([
-                'phone' => fake()->phoneNumber(),
-                'email' => fake()->email(),
-                'notes' => fake()->paragraph(),
+                'phone' => '07' . $faker->numerify('#########'), // Creates a number like '07123456789'
+                'email' => $faker->email,
+                'notes' => $faker->paragraph(),
             ]);
         }
     }
